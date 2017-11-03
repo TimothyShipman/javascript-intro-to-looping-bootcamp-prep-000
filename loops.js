34c54ba1e768109d4cced1689994459cc2c3aa8d
Tim Shipman <bricklayertim@yahoo.com>
2017-11-03 19:51:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,8 +17,11 @@ return ('done')
 }
 
 function doWhileLoop(array) {
-  do-while{
-    while
+  function maybeTrue() {
+    return Math.random() >= 0.5
   }
-  return
+  do {
+    array = array.slice(1)
+} while (array.length > 0 && maybeTrue())
+return array
 }
